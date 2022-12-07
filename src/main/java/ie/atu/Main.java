e50c110e54d6e3e714ccaf9855c592a5ea68f9bb
MaghnusOD <g00377514@atu.ie>
2022-12-07 10:31:26 +0000

    added text

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args){
         Employee employee = new Employee();
 
+        // employee info
         employee.setUsr_name("Morgan");
         employee.setUsr_ppsid("G24852");
         employee.setUsr_phone("08712345678");
